46f6644c8d80fa08133beebfd3547dc09ee27274
liyihwa <liyihwa04@163.com>
2023-09-24 15:31:54 +0800

    1

--- windows.py
+++ windows.py
@@ -35,9 +35,9 @@ def get_score():
         version_idx = len(versions) - 1
     version_score = (len(versions) - version_idx) / len(versions) * 100
 
-
     types = {
         '家庭普通版': 1,
+        '家庭中文版': 1,
         'IoT': 1,
         '教育版': 1.15,
         '企业版': 1.3,
@@ -46,7 +46,8 @@ def get_score():
     }
 
     typ = windows_type()
-    if typ in types.keys():
+    for k in types.keys():
+        if typ in k or k in typ:
         return types[typ] * version_score
     else:
         print("No type as {}".format(typ))
